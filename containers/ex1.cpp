3e9bf81c8cf88d7dbdf4db6dff05ffc18c2bdc6a
Daniil <danast_2@mail.ru>
2024-02-18 19:00:40 +0300

    test2

--- containers/ex1.cpp
+++ containers/ex1.cpp
@@ -3,3 +3,16 @@
 #include <iostream>
 #include <algorithm>
 
+using namespace std;
+
+int main() {
+    std::vector<int> v{1, 2, 3, 4, 5}; // Создаем вектор с элементами 1, 2, 3, 4, 5
+
+    // Выводим содержимое вектора
+    for (int i : v) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    return 0; // Возвращаем 0, чтобы указать успешное завершение программы
+}
