e82556cb420e17b36a2b41bc260d78c7790a4d9a
Daniil <danast_2@mail.ru>
2024-02-18 19:09:41 +0300

    test3

--- containers/ex1.cpp
+++ containers/ex1.cpp
@@ -7,12 +7,20 @@ using namespace std;
 
 int main() {
     std::vector<int> v{1, 2, 3, 4, 5}; // Создаем вектор с элементами 1, 2, 3, 4, 5
+    //Удалим некоторые элементы. Что именно? В нашем примере много значений 2,
+    //удалим их:
+    const auto new_end (remove(begin(v), end(v), 2));
 
-    // Выводим содержимое вектора
-    for (int i : v) {
-        std::cout << i << " ";
+    //укоротим размер вектора:
+    v.erase(new_end, end(v));
+
+    for (auto i :v) {
+        cout << i;
+        if (i != *(v.end())){
+            cout << ", ";
+        }
+
     }
-    std::cout << std::endl;
 
     return 0; // Возвращаем 0, чтобы указать успешное завершение программы
 }
