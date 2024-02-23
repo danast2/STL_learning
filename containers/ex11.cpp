de2713f5380b7b89b89c655846e305b8721ed39d
Daniil <danast_2@mail.ru>
2024-02-23 12:05:41 +0300

    23.02.2024/12:05

---
+++ containers/ex11.cpp
@@ -0,0 +1,18 @@
+//Подсчитываем частоту встречаемости слов
+//с применением контейнера std::map
+
+#include <iostream>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+
+using namespace std;
+
+string filter_punctuation(){
+    const char* forbidden(){/**/};
+}
+
+int main(){
+    return 0;
+}
