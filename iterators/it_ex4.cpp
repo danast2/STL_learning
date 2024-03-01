cc5ad947c156d24c9a6d0c2b5e57e8b6f8dc348a
Daniil <danast_2@mail.ru>
2024-03-01 05:39:02 +0300

    01.03.2024/05:38

--- iterators/it_ex4.cpp
+++ iterators/it_ex4.cpp
@@ -1,3 +1,31 @@
-//
-// Created by Даниил Асташов on 29.02.2024.
-//
+//Реализуем алгоритмы с помощью итераторов
+
+#include <iostream>
+
+class fibit{
+    size_t i{0};
+    size_t a{0};
+    size_t b{1};
+public:
+    fibit() = default;
+    explicit fibit(size_t i_):i{i_} {}
+    size_t operator*() const;
+    fibit& operator++();
+    bool operator!=(const fibit &o) const;
+};
+
+bool fibit::operator!=(const fibit &o) const {
+    return i != o.i;
+}
+
+size_t fibit::operator*() const {
+    return b;
+}
+
+fibit& fibit::operator++(){
+    const size_t old_b{b};
+    b+=a;
+    a = old_b;
+    ++i;
+    return *this;
+}
