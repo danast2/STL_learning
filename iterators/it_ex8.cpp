2d3bb7ac366431f1da9ddb1550d5ed4798781091
Daniil <danast_2@mail.ru>
2024-03-02 07:43:19 +0300

    02.03.2024/07:43

--- iterators/it_ex8.cpp
+++ iterators/it_ex8.cpp
@@ -66,5 +66,10 @@ int main(){
     vector<double> a {1.0, 2.0, 3.0};
     vector<double> b {4.0, 5.0 , 6.0};
     zipper zipped {a, b};
+    const auto add_product ([](double sum, const auto & p) {
+        return sum + p.first * p.second;
+    });
+    const auto dot_product (accumulate(begin(zipped), end(zipped), 0.0, add_product));
+    cout << dot_product << '\n';
     return 0;
 }
