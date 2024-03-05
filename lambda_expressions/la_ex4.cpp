d018dd369bf91888a4e2057fecfad43f662a8d54
Daniil <danast_2@mail.ru>
2024-03-05 05:58:05 +0300

    05.03.2024/05:58

--- lambda_expressions/la_ex4.cpp
+++ lambda_expressions/la_ex4.cpp
@@ -6,7 +6,7 @@
 #include <functional>
 #include <algorithm>
 
-static bool begin_with_a(const std::string &s)
+static bool begins_with_a(const std::string &s)
 {
     return s.find('a') == 0;
 }
@@ -21,3 +21,11 @@ template <class A, class B, class F> auto combine(F binary_func, A a, B b){
     };
 }
 
+using namespace std;
+
+int main(){
+    auto a_xxx_b (combine(logical_and<>{}, begins_with_a, ends_with_b));
+    copy_if(istream_iterator<string>{cin}, {}, ostream_iterator<string>{cout, " "}, a_xxx_b);
+    cout << '\n';
+    return 0;
+}
