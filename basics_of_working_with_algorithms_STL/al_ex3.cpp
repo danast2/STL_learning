9da3ba4cd20b311eb995c121a1f0648f6a8a8701
Daniil <danast_2@mail.ru>
2024-03-08 22:04:10 +0500

    08.03.2024/22:04

---
+++ basics_of_working_with_algorithms_STL/al_ex3.cpp
@@ -0,0 +1,15 @@
+//сортируем контейнеры
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <iterator>
+#include <random>
+
+using namespace std;
+
+
+
+int main(){
+    return 0;
+}
