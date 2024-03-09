ace0970a43562662838ea4888a7d323ffba6dc97
Daniil <danast_2@mail.ru>
2024-03-09 23:19:45 +0500

    09.03.2024/23:19

--- basics_of_working_with_algorithms_STL/ex_al4.cpp
+++ basics_of_working_with_algorithms_STL/ex_al4.cpp
@@ -5,4 +5,38 @@
 #include <algorithm>
 #include <iterator>
 
-void print(){}
+void print(const std::vector<int> &v){
+    std::copy(std::begin(v), std::end(v), std::ostream_iterator<int>{std::cout, " "});
+    std::cout << '\n';
+}
+
+int main(){
+    std::vector<int> v {1, 2, 3, 4, 5, 6};
+    print(v);
+    {
+        const auto new_end (remove(begin(v), end(v), 2));
+        v.erase(new_end, end(v));
+    }
+    print(v);
+    {
+        auto odd_number ([](int i) { return i % 2 != 0; });
+        const auto new_end (
+        remove_if(begin(v), end(v), odd_number));
+        v.erase(new_end, end(v));
+    }
+    print(v);
+    replace(begin(v), end(v), 4, 123);
+    print(v);
+    v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    std::vector<int> v2;
+    std::vector<int> v3;
+    auto odd_number ([](int i) { return i % 2 != 0; });
+    auto even_number ([](int i) { return i % 2 == 0; });
+    remove_copy_if(begin(v), end(v),
+                   back_inserter(v2), odd_number);
+    copy_if(begin(v), end(v),
+            back_inserter(v3), even_number);
+    print(v2);
+    print(v3);
+    return 0;
+}
